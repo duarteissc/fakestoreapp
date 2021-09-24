34b557cbbce18bcf3c3d34f26177d5bee54104e5
D <duarte.issc@gmail.com>
2021-09-23 19:42:17 -0500

    add
    Add

--- pages/cart.js
+++ pages/cart.js
@@ -12,6 +12,7 @@ const Cart = ({ cart, addProductCar, deleteProductsCar, deleteProductCar }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossOrigin="anonymous" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossOrigin="anonymous"></script>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavBar cart={cart} type={0}/>
       
